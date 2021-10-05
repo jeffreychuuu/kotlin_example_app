a93067845ddec372926ab10ebc0d4db115e2809a
Jeffrey Chu <jeffreychu888hk@gmail.com>
2021-10-06 00:52:13 +0800

    deplopyment test

--- mongo_service/src/main/kotlin/com/example/mongo_service/MongoServiceApplication.kt
+++ mongo_service/src/main/kotlin/com/example/mongo_service/MongoServiceApplication.kt
@@ -7,6 +7,7 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class MongoServiceApplication
 
+//test
 fun main(args: Array<String>) {
     SpringApplication.run(MongoServiceApplication::class.java, *args)
 }
