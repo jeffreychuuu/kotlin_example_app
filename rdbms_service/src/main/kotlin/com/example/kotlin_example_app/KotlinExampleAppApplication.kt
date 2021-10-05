648ed8cc150f12d183361c18cd0ddc35990912eb
Jeffrey Chu <jeffreychu888hk@gmail.com>
2021-10-05 21:18:50 +0800

    update circleci workflow - 49

--- rdbms_service/src/main/kotlin/com/example/kotlin_example_app/KotlinExampleAppApplication.kt
+++ rdbms_service/src/main/kotlin/com/example/kotlin_example_app/KotlinExampleAppApplication.kt
@@ -13,7 +13,7 @@ fun listen(input: String?) {
     println("input value: $input")
 }
 
-//te
+//tes
 fun main(args: Array<String>) {
     SpringApplication.run(KotlinExampleAppApplication::class.java, *args)
 }
