6db0410ac9d70daa7cee29d537ff25b4b15993e2
Jeffrey Chu <jeffreychu888hk@gmail.com>
2021-10-05 23:03:10 +0800

    add cache to circle ci - 9

--- mongo_service/src/main/kotlin/com/example/mongo_service/MongoServiceApplication.kt
+++ mongo_service/src/main/kotlin/com/example/mongo_service/MongoServiceApplication.kt
@@ -7,7 +7,6 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class MongoServiceApplication
 
-//test
 fun main(args: Array<String>) {
     SpringApplication.run(MongoServiceApplication::class.java, *args)
 }
