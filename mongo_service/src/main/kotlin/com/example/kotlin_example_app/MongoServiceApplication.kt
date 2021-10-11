143d81a181e09a6bd26eed724574a9a10e67eb5d
Jeffrey Chu <jeffrey.chu@leightonasis.com>
2021-10-11 10:12:24 +0800

    edit circleci workflow

--- mongo_service/src/main/kotlin/com/example/kotlin_example_app/MongoServiceApplication.kt
+++ mongo_service/src/main/kotlin/com/example/kotlin_example_app/MongoServiceApplication.kt
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 class MongoServiceApplication
 
+// test
 fun main(args: Array<String>) {
     SpringApplication.run(MongoServiceApplication::class.java, *args)
 }
