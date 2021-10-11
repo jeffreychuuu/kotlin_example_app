8dbe23c2f63ff64e9d1d0430e04d7f59c4268382
Jeffrey Chu <jeffrey.chu@leightonasis.com>
2021-10-11 15:42:18 +0800

    update version of protobuf-gradle-plugin

--- grpc_lib/build.gradle.kts
+++ grpc_lib/build.gradle.kts
@@ -2,15 +2,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.google.protobuf.gradle.*;
 
 buildscript {
-	dependencies {
-		classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.13")
-	}
+    dependencies {
+        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.17")
+    }
 }
 
 plugins {
-	id("com.google.protobuf") version "0.8.13"
+    id("com.google.protobuf") version "0.8.17"
 //	kotlin("jvm") version "1.4.32" // remove the version tag if you are build by other project
-	kotlin("jvm")
+    kotlin("jvm")
 }
 
 group = "com.kotlingrpc"
@@ -18,52 +18,53 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 allprojects {
-	repositories {
-		mavenLocal()
-		mavenCentral()
-		google()
-	}
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        google()
+    }
 }
 
 
 dependencies {
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	compileOnly("javax.annotation:javax.annotation-api:1.3.2")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	// gRPC
-	api("io.grpc:grpc-kotlin-stub:0.2.1")
-	implementation("io.grpc:protoc-gen-grpc-kotlin:1.1.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    compileOnly("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    // gRPC
+    api("io.grpc:grpc-kotlin-stub:0.2.1")
+    implementation("io.grpc:protoc-gen-grpc-kotlin:1.1.0")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
 
-	}
+    }
 }
 
 protobuf {
-	protoc{
-		artifact = "com.google.protobuf:protoc:3.14.0" // you need to mention :osx-x86_64 behind if you are using m1 mac
-	}
-	generatedFilesBaseDir = "$projectDir/src/main/kotlin/generated"
-	plugins {
-		id("grpc"){
-			artifact = "io.grpc:protoc-gen-grpc-java:1.36.0" // you need to mention :osx-x86_64 behind if you are using m1 mac
-		}
-		id("grpckt") {
-			artifact = "io.grpc:protoc-gen-grpc-kotlin:1.1.0:jdk7@jar"
-		}
-	}
-	generateProtoTasks {
-		ofSourceSet("main").forEach { generateProtoTask ->
-			generateProtoTask
-				.plugins {
-					id("grpc")
-					id("grpckt")
-				}
-		}
-	}
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.14.0" // you need to mention :osx-x86_64 behind if you are using m1 mac
+    }
+    generatedFilesBaseDir = "$projectDir/src/main/kotlin/generated"
+    plugins {
+        id("grpc") {
+            artifact =
+                "io.grpc:protoc-gen-grpc-java:1.36.0" // you need to mention :osx-x86_64 behind if you are using m1 mac
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.1.0:jdk7@jar"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach { generateProtoTask ->
+            generateProtoTask
+                .plugins {
+                    id("grpc")
+                    id("grpckt")
+                }
+        }
+    }
 }
